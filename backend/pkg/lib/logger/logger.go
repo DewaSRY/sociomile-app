817af4ed82f33a36190388c42f5bde6de4d47f80
DewaSRY <sdewa6645@gmail.com>
2026-02-12 20:07:50 +0800

    init: logger

---
+++ backend/pkg/lib/logger/logger.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Log *zap.Logger
+
+func Init() {
+	env := os.Getenv("APP_ENV")
+
+	var config zap.Config
+
+	if env == "production" {
+		config = zap.NewProductionConfig()
+	} else {
+		config = zap.NewDevelopmentConfig()
+	}
+
+	// Better timestamp format
+	config.EncoderConfig.TimeKey = "time"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	logger, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	Log = logger
+}
